573ead8926e92add4c02aae1f814df27cbff398b
ybuiko <juliabuiko@gmail.com>
2024-03-01 09:04:10 -0500

    seed created

--- models/seed.js
+++ models/seed.js
@@ -1,55 +1,74 @@
-const mongoose = require("./connection")
+const mongoose = require("./connection");
 // const express = require("express")
-const Deck = require("./deck")
-const Card = require("./cards")
+const Deck = require("./deck");
+const Card = require("./cards");
 
+mongoose.connection.on("open", async () => {
+  const startDecks = [
+    {
+      name: "MackWick",
+      isPrivate: false,
+      username: "seed",
+      description: "this is the mackwick deck",
+      cards: [],
+    },
+    {
+      name: "Craig David",
+      isPrivate: false,
+      username: "seed",
+      description: "Can you fill me in?",
+      cards: [],
+    },
+  ];
 
-mongoose.connection.on("open", async (req, res) => {
-    try{
-        const startDecks = [
-            {
-                name: "MackWick",
-                isPrivate: false,
-                username: "The Mackwicker",
-                description: "this is the mackwick deck",
-                // cards: [{ type: mongoose.Types.ObjectId, ref: "Card" }],
-              },
-              {
-                name: "Craig David",
-                isPrivate: false,
-                username: "David Craig",
-                description: "Can you fill me in?",
-                // cards: [{ type: mongoose.Types.ObjectId, ref: "Card" }],
-              },
-]
+  await Deck.deleteMany({});
+  await Card.deleteMany({});
 
-        const startCards = [
-            {
-                username: "String",
-                question: "Not",
-                answer: "answer",
-                // deckId: mongoose.Types.ObjectId,
-              },
-              {
-                username: "2",
-                question: "much",
-                answer: "sauce",
-                // deckId: mongoose.Types.ObjectId,
-              },
-        ]
+  //create deck data
+  const deckData = await Deck.create(startDecks);
 
-        await Deck.deleteMany({})
-        await Card.deleteMany({})
-        const data = await Deck.create(startDecks)
-        const dataTwo = await Card.create(startCards)
-        console.log(data)
-        console.log(dataTwo)
-        mongoose.connection.close()
+  //create card data and pass deck ids
+  const startCards = [
+    {
+      username: "seed",
+      question: "Not",
+      answer: "answer",
+      deckId: deckData[0]._id,
+    },
+    {
+      username: "seed",
+      question: "much",
+      answer: "sauce",
+      deckId: deckData[0]._id,
+    },
+    {
+      username: "seed",
+      question: "Not",
+      answer: "answer",
+      deckId: deckData[1]._id,
+    },
+    {
+      username: "seed",
+      question: "much",
+      answer: "sauce",
+      deckId: deckData[1]._id,
+    },
+  ];
 
-    } catch (error) {
-        res.status(400).json(error);
-    }
-})
+  //create cards
+  const cardData = await Card.create(startCards);
+
+  //update decks with card ids
+  deckData[0].cards = cardData.filter((card) => {
+    if (card.deckId === deckData[0]._id) return card._id;
+  });
+  await deckData[0].save();
+  deckData[1].cards = cardData.filter((card) => {
+    if (card.deckId === deckData[1]._id) return card._id;
+  });
+  await deckData[1].save();
+  mongoose.connection.close();
+});
 
 // AOudG9g5t1EKNJEi7Epp3M8KTOg9bgqmzdlYD9wI
 // fetch('https://quizapi.io/api/v1/questions?apiKey=AOudG9g5t1EKNJEi7Epp3M8KTOg9bgqmzdlYD9wI&limit=10');
